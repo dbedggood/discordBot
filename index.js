98a66aef6621d5056b455aa2320cccba33978847
Daniel Bedggood <ddbedggood@gmail.com>
2019-01-15 14:51:41 +1300

    Dad jokes
    Using "I'm" or "I am" in chat triggers the bot to reply
    
    The bot replies with "Hi ______, I'm waery"

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 require('dotenv').config()
 
 const Discord = require('discord.js')
-const client = new Discord.Client()
+const bot = new Discord.Client()
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+bot.login(process.env.BOT_TOKEN)
+
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag}!`)
 })
 
-client.on('message', msg => {
-  if (msg.content === 'yeet') {
-    msg.reply('**dabs**')
+// bot.on('typingStart', (channel) => {
+//   channel.send("shut up")
+// })
+// bot.on('typingStop', (channel) => {
+//   channel.send("good <:waery:534517623115874315>")
+// })
+
+bot.on('message', msg => {
+  console.log(msg.content)
+
+  if (!msg.author.bot) {
+
+    if (msg.content.match(/i'm/i)) {
+      msg.channel.send("Hi " + msg.content.split(/i'm /i)[1] + ", I'm waery <:waery:534517623115874315>");
+    } else if (msg.content.match(/i am/i)) {
+      msg.channel.send("Hi " + msg.content.split(/i am /i)[1] + ", I'm waery <:waery:534517623115874315>");
+    }
+  
   }
-})
 
-client.login(process.env.BOT_TOKEN)
+})
