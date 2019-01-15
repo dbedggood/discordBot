ff7a18a05096d1240a0ff5b864e8266e832e71b2
Daniel Bedggood <ddbedggood@gmail.com>
2019-01-16 10:03:00 +1300

    Add youtube search function
    Allows users to search a video and choose from a list
    
    The bot then returns a URL of the selected video

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 
+const Youtube = require('simple-youtube-api')
 const Discord = require('discord.js')
+
+const youtube = new Youtube(process.env.GOOGLE_API_KEY)
 const bot = new Discord.Client()
 
 bot.login(process.env.BOT_TOKEN)
@@ -17,7 +20,7 @@ bot.on('ready', () => {
 // })
 
 bot.on('message', msg => {
-  console.log(msg.content)
+  // console.log(msg.content)
 
   if (!msg.author.bot) {
 
@@ -26,7 +29,42 @@ bot.on('message', msg => {
     } else if (msg.content.match(/i am/i)) {
       msg.channel.send("Hi " + msg.content.split(/i am /i)[1] + ", I'm waery <:waery:534517623115874315>");
     }
-  
+
+    if (msg.content.match(/yt/i)) {
+      youtube.searchVideos(msg.content.split(/yt /i)[1], 5)
+        .then(results => {
+          msg.channel.send('Reply with 1-5 to choose a video: \n'
+                            + '1 - ' + results[0].title + '\n'
+                            + '2 - ' + results[1].title + '\n'
+                            + '3 - ' + results[2].title + '\n'
+                            + '4 - ' + results[3].title + '\n'
+                            + '5 - ' + results[4].title)
+          
+          const collector = new Discord.MessageCollector(msg.channel, m => m.author.id === msg.author.id, { time: 10000 });
+          collector.on('collect', message => {
+              switch (message.content) {
+                case '1':
+                  message.channel.send(results[0].url)
+                  break;
+                case '2':
+                  message.channel.send(results[1].url)
+                  break;
+                case '3':
+                  message.channel.send(results[2].url)
+                  break;
+                case '4':
+                  message.channel.send(results[3].url)
+                  break;
+                case '5':
+                  message.channel.send(results[4].url)
+                  break;
+                default:
+                  break;
+            }
+          })
+        })
+        .catch(console.log);
+    }
   }
 
 })
